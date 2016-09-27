33fafc329531960b62521a3ad92abba99e7cf4ab
Vardan <vard.1995@gmail.com>
2016-09-27 20:14:28 +0400

    adds inputs active

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 
 
- $('.form__row__label').click(function() {
-     .form__row__label = $(this).attr('for');
-     $('.'+ form__row__label).trigger('click');
- });
+ $('.form__row__label').on((click),function() { });
+ function checkbox () {
+     var check = document.getElementById(".form__row__label").checked;
+     var box = document.getElementById("form__row__label")
+
+     if (check == true) {
+         box.checked = false;
+     }
+     else if (check == false) {
+         box.checked = true;
+     }
+ }
