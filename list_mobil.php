29ba743b8e5f5c40d420f017cba3c59d7eb222ae
Gagik <gagik@bigbek.com>
2016-11-03 00:41:52 +0100

    minor fix

--- list_mobil.php
+++ list_mobil.php
@@ -1006,17 +1006,23 @@ $body = "From: " . $_POST["input_0"] . "\n E-Mail: " . $_POST["input_00"] . "\n 
 
 if ($captcha !== '' && $_POST["input_0"] !== '' && $_POST["input_00"] !== '') {
     if (mail($to, $subject, $body, $from)) {
-        echo '<h1 style="text-align: center;">Votre message a été envoyé!</h1>';
+        echo '<h2 style="text-align: center;">Votre message a été envoyé!</h2>';
     } else {
-        echo '<h1 style="text-align: center;">Quelque chose s\'est mal passé, revenir en arrière et essayer</h1>';
+        echo '<h2 style="text-align: center;">Quelque chose s\'est mal passé, revenir en arrière et essayer</h2>';
     }
-} else if ($captcha !== '') {
-    echo '<h1 style="text-align: center;">Vous avez répondu à la question anti-spam</h1>';
-    echo '<h1 style="text-align: center;"> <a href="javascript:history.back()">Aller a la page precedent</a></h1>';
-} else if ($_POST["input_0"] !== '') {
-    echo '<h1 style="text-align: center;">Vous n\'avez pas rempli le champ obligatoire pour le nom</h1>';
-    echo '<h1 style="text-align: center;"> <a href="javascript:history.back()">Aller a la page precedent</a></h1>';
-} else if ($_POST["input_00"] !== '') {
-    echo '<h1 style="text-align: center;">Vous n\'avez pas rempli le champ obligatoire pour Email</h1>';
-    echo '<h1 style="text-align: center;"><a href="javascript:history.back()">Aller a la page precedent</a></h1>';
+} else {
+    if ($captcha == '') {
+        echo '<h2 style="text-align: center;">Vous avez répondu à la question anti-spam</h2>';
+    }
+    if ($_POST["input_0"] == '') {
+        echo '<h2 style="text-align: center;">Vous n\'avez pas rempli le champ obligatoire pour le nom</h2>';
+    }
+    if ($_POST["input_00"] == '') {
+        echo '<h2 style="text-align: center;">Vous n\'avez pas rempli le champ obligatoire pour Email</h2>';
+    }
+    echo '<h1 style="text-align: center;"> <a href="javascript:history.back()">Ouvrir la page precedent</a></h1>';
 }
+
+
+
+
